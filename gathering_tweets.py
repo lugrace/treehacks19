c440764f74546a96b67b7414b5e6fb6505a9764f
vaishmantha <ysh.mantha@gmail.com>
2017-11-04 14:47:43 -0400

    college tweets working

--- gathering_tweets.py
+++ gathering_tweets.py
@@ -2,6 +2,7 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import tweepy
 from math import cos, pi
 from geopy.geocoders import Nominatim
 
@@ -12,48 +13,16 @@ consumer_secret = '3b55vyoax4DIkhvTX6KUGP9sSjhxo9ZxOfpUUtsz6tpPVfZfw3'
 access_token = '926830219356340225-z2qjfLCagnxp99AL4UhzQ94LUQbo9RR'
 access_secret = 'uX5IWIi0ERKLySSQEYIVOSIcjEuHCmJlPwEK2zSLLLgGk'
 
-#This is a basic listener that just prints received tweets to stdout.
+tweet_data = []
 
-tweet_data = list()
-class StdOutListener(StreamListener):
-
-    def on_data(self, data):
-        print(data)
-        return True
-
-    def on_error(self, status):
-        print(status)
-
-
-if __name__ == '__main__':
-
-    #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret)
-    stream = Stream(auth, l)
-    stream.filter(track = ['UMD'])
-    stream.filter(count = [1])
-
-import json
-import pandas as pd
-import matplotlib.pyplot as plt
-
-tweets_data = []
-tweets_file = open("tweets.txt", "r")
-for line in tweets_file:
-    try:
-        tweet = json.loads(line)
-        tweets_data.append(tweet)
-    except:
-        continue
-
-print ("tweets here", len(tweets_data))
-
-tweets = pd.DataFrame()
-
-tweets['id'] = map(lambda tweet: tweet.get('id', None),tweets_data)
-tweets['text'] = map(lambda tweet: tweet.get('text', None),tweets_data)
-
-print(tweets.head())
-print(tweets)
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tweepy.API(auth)
+for tweet in tweepy.Cursor(api.search,
+                           q="UMD",
+                           count=100,
+                           result_type="recent",
+                           include_entities=True,
+                           lang="en").items(2500):     # the values inside items defines how many searches we want
+    print(tweet.text)
+    tweet_data.append(tweet.text)
