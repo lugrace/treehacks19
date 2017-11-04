19d748246f78af30098a7dd57c95252934ead320
Grace Lu <2018glu@tjhsst.edu>
2017-11-04 14:14:14 -0400

    added olivias files to framework

--- mysite/happystudent/urls.py
+++ mysite/happystudent/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^home/$', views.index, name='index'),
+    url(r'^home/finder/$', views.finder),
     url(r'^home/college/(?P<college>\w+)/$', views.analyze),
     url(r'^home/ten/$', views.topten),
 ]
--- mysite/happystudent/views.py
+++ mysite/happystudent/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#for the backend part
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API
+consumer_key = 'oeaiWzPmHLx20OLsp5g5QPFbw'
+consumer_secret = '3b55vyoax4DIkhvTX6KUGP9sSjhxo9ZxOfpUUtsz6tpPVfZfw3'
+access_token = '926830219356340225-z2qjfLCagnxp99AL4UhzQ94LUQbo9RR'
+access_secret = 'uX5IWIi0ERKLySSQEYIVOSIcjEuHCmJlPwEK2zSLLLgGk'
+
 # Create your views here.
 def index(request):
 	return render(
@@ -23,4 +34,11 @@ def topten(request):
 	return render(
 		request,
 		'ten.html'
+	)
+
+def finder(request):
+	#do stuff
+	return render(
+		request,
+		'finder.html'
 	)
