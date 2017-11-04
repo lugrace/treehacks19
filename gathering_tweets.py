5c254a83a7e4b4d7ede4db223ceff77c6dcc650a
vaishmantha <ysh.mantha@gmail.com>
2017-11-04 15:55:05 -0400

    working on using api

--- gathering_tweets.py
+++ gathering_tweets.py
@@ -5,9 +5,6 @@ from tweepy import Stream
 import tweepy
 from math import cos, pi
 from geopy.geocoders import Nominatim
-import http.client, urllib.request, urllib.parse, urllib.error, base64
-import http.client, urllib
-import json
 
 
 # TWITTER API CREDENTIALS
@@ -34,35 +31,3 @@ for tweet in tweepy.Cursor(api.search,
 
 
 
-# Replace the accessKey string value with your valid access key.
-accessKey = 'f15920f0f61947c29e12c7f1f12174f9'
-
-# Replace or verify the region.
-#
-# You must use the same region in your REST API call as you used to obtain your access keys.
-# For example, if you obtained your access keys from the westus region, replace
-# "westcentralus" in the URI below with "westus".
-#
-# NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
-# a free trial access key, you should not need to change this region.
-uri = 'westus.api.cognitive.microsoft.com'
-path = '/text/analytics/v2.0/sentiment'
-
-def GetSentiment (documents):
-    "Gets the sentiments for a set of documents and returns the information."
-
-    headers = {'Ocp-Apim-Subscription-Key': accessKey}
-    conn = http.client.HTTPSConnection (uri)
-    body = json.dumps (documents)
-    conn.request ("POST", path, body, headers)
-    response = conn.getresponse ()
-    return response.read()
-
-documents = { 'documents': [
-    { 'id': '1', 'language': 'en', 'text': 'I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.' },
-    { 'id': '2', 'language': 'es', 'text': 'Este ha sido un dia terrible, llegué tarde al trabajo debido a un accidente automobilistico.' }
-]}
-
-
-result = GetSentiment (documents)
-print (json.dumps(json.loads(result), indent=4))
