fe2ed8bc27bde20449737f7a2a7c4ead9d13ea74
Grace Lu <2018glu@tjhsst.edu>
2017-11-04 13:36:39 -0400

    more views and routes

--- mysite/happystudent/urls.py
+++ mysite/happystudent/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^home/$', views.index, name='index'),
+    url(r'^home/college/(?P<college>\w+)/$', views.analyze),
+    url(r'^home/ten/$', views.topten),
 ]
--- mysite/happystudent/views.py
+++ mysite/happystudent/views.py
@@ -8,3 +8,19 @@ def index(request):
         'index.html'
     )
     # return HttpResponse("Hello, world. You're at the polls index.")
+
+def analyze(request, college="University of Maryland"):
+	#do stuff
+
+	return render(
+		request, 
+		'results.html',
+		context={'college_name':college}
+	)
+
+def topten(request):
+	#do stuff
+	return render(
+		request,
+		'ten.html'
+	)
