dda315331c30d017364cfbed33df2067fe0a2f9d
vaishmantha <ysh.mantha@gmail.com>
2017-11-04 18:56:24 -0400

    added key phrases

--- sentiment_analysis_test.py
+++ sentiment_analysis_test.py
@@ -16,6 +16,7 @@ accessKey = 'f15920f0f61947c29e12c7f1f12174f9'
 # a free trial access key, you should not need to change this region.
 uri = 'westcentralus.api.cognitive.microsoft.com'
 path = '/text/analytics/v2.0/sentiment'
+path2 = '/text/analytics/v2.0/keyPhrases'
 
 def GetSentiment (documents):
     "Gets the sentiments for a set of documents and returns the information."
@@ -27,11 +28,21 @@ def GetSentiment (documents):
     response = conn.getresponse ()
     return response.read()
 
-documents = { 'doucments': [
+def GetKeyWords (documents):
+    headers = {'Ocp-Apim-Subscription-Key': accessKey}
+    conn = http.client.HTTPSConnection (uri)
+    body = json.dumps (documents)
+    conn.request ("POST", path2, body, headers)
+    response = conn.getresponse ()
+    return response.read()
+documents = { 'documents': [
     { 'id': '1', 'language': 'en', 'text': 'I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.' },
+    { 'id': '2', 'language': 'en', 'text': 'There is so much discrimination in this world.' }
 ]}
 
 
 result = GetSentiment(documents)
+result2 = GetKeyWords(documents)
 print(result)
+print(result2)
 #print (json.dumps(json.loads(result), indent=4))
