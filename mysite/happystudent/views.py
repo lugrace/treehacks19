096d367281f3e5cd17c2e3653ba0050ce2053a38
Grace Lu <2018glu@tjhsst.edu>
2017-11-05 00:31:55 -0400

    comments

--- mysite/happystudent/views.py
+++ mysite/happystudent/views.py
@@ -109,12 +109,6 @@ def analyzeForm(request):
 
 	return render(request, 'finder.html', {'form': form})
 
-def topten(request):
-	#do stuff
-	return render(
-		request,
-		'ten.html'
-	)
 
 def finder(request):
 	#do stuff
@@ -152,58 +146,58 @@ def GetKeyWords (documents):
     return response.read()
 
 def topTen(request):
-	f = open("static/txt/college_acronyms.txt", 'r', encoding="utf8")
-	colleges = {}
-	top_ten = {}
-	line = f.readline()
-	while line != "":
-	    line = line.split(' - ')
-	    fullname = line[1][:-1].split(', ')
-	    for x in fullname:
-	        colleges[x] = line[0]
-	    line = f.readline()
+	# f = open("static/txt/college_acronyms.txt", 'r', encoding="utf8")
+	# colleges = {}
+	# top_ten = {}
+	# line = f.readline()
+	# while line != "":
+	#     line = line.split(' - ')
+	#     fullname = line[1][:-1].split(', ')
+	#     for x in fullname:
+	#         colleges[x] = line[0]
+	#     line = f.readline()
 
-	for input_word in colleges:
-	    acronym_word = colleges[input_word]
-	    tweet_data = queue.Queue()
+	# for input_word in colleges:
+	#     acronym_word = colleges[input_word]
+	#     tweet_data = queue.Queue()
 
-	    auth = OAuthHandler(consumer_key, consumer_secret)
-	    auth.set_access_token(access_token, access_secret)
-	    api = tweepy.API(auth)
+	#     auth = OAuthHandler(consumer_key, consumer_secret)
+	#     auth.set_access_token(access_token, access_secret)
+	#     api = tweepy.API(auth)
 
-	    for tweet in tweepy.Cursor(api.search, q=input_word, count=3500, result_type="recent", include_entities=True, lang="en").items(100):     # the values inside items defines how many searches we want
-	        tweet_data.put(tweet.text)
-	    for tweet in tweepy.Cursor(api.search, q=acronym_word, count=3500, result_type="recent", include_entities=True, lang="en").items(100):     # the values inside items defines how many searches we want
-	        tweet_data.put(tweet.text)
-	    tweetlist = []
+	#     for tweet in tweepy.Cursor(api.search, q=input_word, count=3500, result_type="recent", include_entities=True, lang="en").items(100):     # the values inside items defines how many searches we want
+	#         tweet_data.put(tweet.text)
+	#     for tweet in tweepy.Cursor(api.search, q=acronym_word, count=3500, result_type="recent", include_entities=True, lang="en").items(100):     # the values inside items defines how many searches we want
+	#         tweet_data.put(tweet.text)
+	#     tweetlist = []
 
-	    x = 1
-	    while tweet_data.empty() == False:
-	        tweetlist.append({'id': str(x), 'language': 'en', 'text': str(tweet_data.get())})
-	        x+=1
-	    documents = { 'documents': tweetlist }
+	#     x = 1
+	#     while tweet_data.empty() == False:
+	#         tweetlist.append({'id': str(x), 'language': 'en', 'text': str(tweet_data.get())})
+	#         x+=1
+	#     documents = { 'documents': tweetlist }
 
-	    percentresults = eval(GetSentiment(documents))
-	    avpercent = 0.0
-	    positive = 0.0
-	    negative = 0.0
-	    for z in percentresults["documents"]:
-	        avpercent += z["score"]
-	        if z["score"] < 0.01:
-	            negative+=1
-	        elif z["score"] > 0.8:
-	            positive+=1
-	    avpercent = avpercent/len(percentresults["documents"])
-	    top_ten.update({input_word: avpercent})
-	sorted_x = sorted(top_ten.items(), key=operator.itemgetter(1))
+	#     percentresults = eval(GetSentiment(documents))
+	#     avpercent = 0.0
+	#     positive = 0.0
+	#     negative = 0.0
+	#     for z in percentresults["documents"]:
+	#         avpercent += z["score"]
+	#         if z["score"] < 0.01:
+	#             negative+=1
+	#         elif z["score"] > 0.8:
+	#             positive+=1
+	#     avpercent = avpercent/len(percentresults["documents"])
+	#     top_ten.update({input_word: avpercent})
+	# sorted_x = sorted(top_ten.items(), key=operator.itemgetter(1))
 
-	returnme = []
-	for rrr in sorted_x.keys()[0:10]:
-		returnme.append(rrr)
-		returnme.append(sorted_x[rrr])
+	# returnme = []
+	# for rrr in sorted_x.keys()[0:10]:
+	# 	returnme.append(rrr)
+	# 	returnme.append(sorted_x[rrr])
 
 	return render(
 		request, 
-		'results.html',
-		context={'returnme': returnme}
+		'ten.html',
+		# context={'returnme': returnme}
 	)
