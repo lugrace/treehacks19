a33153b1b694fb22937c89fea856acd5df948e0f
ptes77 <31578309+ptes77@users.noreply.github.com>
2019-02-16 15:00:57 -0800

    Temporary push with early classification method

--- classifierdemo/vision/vision1.py
+++ classifierdemo/vision/vision1.py
@@ -10,29 +10,141 @@ client = vision.ImageAnnotatorClient()
 file_prefix = 'C:\\Users\\eshao\\Documents\\Caltech\\treehacks2019\\'
 pic_prefix = 'C:\\Users\\eshao\\Pictures\\treehacks2019\\'
 
-file_string_name = pic_prefix + 'menu.png'
-file_name = os.path.join(os.path.dirname(__file__), file_string_name)
+def rating(factors):
+    '''
+    Returns the environmental rating of a food given the significant factors
+    the food has in respect to the environment.
+    
+    Arguments:
+      - A list of factors that contribute CO2 to the environment
+    
+    Return Value:
+      - A float score that represents the final CO2 score rating
+    '''
+    return 1
 
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+def print_results(food_items):
+    '''
+    Prints out the food items in order based on the inputed food array
+    
+    Arguments:
+      - An array of food items (should be already sorted)
+    
+    Return Value:
+      - None
+    '''
+    # Potential animation from the original location to the ranked location?
+    print('Sorted from most environmentally friendly to least ' + \
+          'environmentally friendly food choice')
+    
+    for food in food_items:
+        print(list_to_string(food))
+        print()
 
-image = types.Image(content=content)
+def list_to_string(lst):
+    '''
+    Converts a list of food descriptions into a string.
+    
+    Arguments:
+      - A list containing food items
+    
+    Return Value:
+      - A string containing the description of the food item
+    '''
+    output = ''
+    for i in range(len(lst) - 1):
+        output += lst[i] + ', '
+    output += lst[len(lst) - 1]
+    
+    return output
 
-response = client.label_detection(image=image)
-labels = response.label_annotations
-
-text_response = client.text_detection(image=image)
-texts = text_response.text_annotations
-
-print(file_name)
-print('Classifications:')
-for classification in labels:
-    print(f'Description: {classification.description}, \t' + \
-          f'Topicality: {classification.topicality}')   
-
-print('Texts:')
-for text in texts:
-    print(f'\n{text.description}')
-    print('lol')
-    vertices = ['({},{})'.format(vertex.x, vertex.y) for vertex in text.bounding_poly.vertices]
-    print(f'bounds: {",".join(vertices)}')
+def classify(image_name):
+    '''
+    Takes in an image name and return the food and the environmental effects
+    each food has on the environment.
+    
+    Arguments:
+      - Image name (e.g. 'sust.jpg')
+    
+    Return Value:
+      - List of food objects ranked in best to worst for the environment
+    '''
+    file_string_name = pic_prefix + image_name
+    file_name = os.path.join(os.path.dirname(__file__), file_string_name)
+    
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+    
+    image = types.Image(content=content)
+    
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    
+    text_response = client.text_detection(image=image)
+    texts = text_response.full_text_annotation
+    
+    print(file_name)
+    '''
+    print('Classifications:')
+    for classification in labels:
+        print(f'Description: {classification.description}, \t' + \
+              f'Topicality: {classification.topicality}')
+    '''
+    
+    # Obtaining the items of food on the menu
+    pages = texts.pages
+    food_items = []
+    ratings = []
+    
+    for page in pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                words = []
+                bounds = paragraph.bounding_box
+                for word in paragraph.words:
+                    curr_word = ""
+                    for symbol in word.symbols:
+                        curr_word += symbol.text
+                    words.append(curr_word)
+            if len(words) > 1:
+                food_items.append((bounds, words))
+            elif len(words) == 1:
+                try:
+                    num = int(words[0])
+                except:
+                    food_items.append((bounds, words))
+                    
+    # food_items:
+    # (four vertices starting top left clockwise, list of words representing food)
+    
+    for food in food_items:
+        raw_description = food[1]
+        factors = []
+        for description in raw_description:
+            # If description in the database
+            # if description in {DATABASE NAME}:
+            #     factors.append(description)
+        # Problem is that WE DON'T KNOW HOW TO DISTINGUISH THE TITLE FROM ELSE
+        # Potential NLP that determines when the title stops and the
+        # description starts?
+        
+        # NLP to denote certain words (e.g. organic, processed, and add extra
+        # to the rating or not
+        ratings.append(rating(factors))
+    
+    # Low rating is good
+    sorted_food = [x for _, x in sorted(zip(ratings, food_items))]
+    print_results(sorted_food[1])
+    
+        
+    
+    
+    
+    '''
+    # Prints out 1) the entire text and 2) each vertex of each word in the text
+    print('Texts:')
+    for text in texts:
+        print(f'\n{text.description}')
+        vertices = ['({},{})'.format(vertex.x, vertex.y) for vertex in text.bounding_poly.vertices]
+        print(f'bounds: {",".join(vertices)}')
+    '''
