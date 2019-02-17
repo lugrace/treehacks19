c6aa2edfad8fc2c6983ff2fa3bb147078e6e7a04
KevinHuang8 <33139341+KevinHuang8@users.noreply.github.com>
2019-02-17 11:18:57 -0800

    Update forms.py

--- mysite/happystudent/forms.py
+++ mysite/happystudent/forms.py
@@ -19,4 +19,4 @@ class MultipleUploadFileForm(forms.Form):
 	water_use = forms.CharField(max_length=50, required=False)
 	co2 = forms.CharField(max_length=50, required=False)
 	land_use = forms.CharField(max_length=50, required=False)
-	file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+	upload_files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
