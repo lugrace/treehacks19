04fde0b1685f2c2272dcb4eb219339a162a7e103
Kevin Huang <kehuang8@gmail.com>
2019-02-16 16:08:46 -0800

    actual machine learning with azure custom vision

--- classifierdemo/testapp/views.py
+++ classifierdemo/testapp/views.py
@@ -1,6 +1,6 @@
 import sys
 import os
-DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DIR = os.path.dirname(os.path.abspath(__file__))
 print(os.path.join(DIR, r"vision\treehacks-food-recognizer-3787a7fb5f64.json"))
 
 from django.http import HttpResponseRedirect
@@ -11,9 +11,10 @@ import io
 from google.cloud import vision
 from google.cloud.vision import types
 
-client = vision.ImageAnnotatorClient()
+from . import predictor 
 
 def classify(image_file):
+    client = vision.ImageAnnotatorClient()
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(DIR, r"vision\treehacks-food-recognizer-3787a7fb5f64.json")
 
     content = image_file.read()
@@ -30,8 +31,14 @@ def upload_file(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             file = request.FILES['file']
+            
+            '''
             labels = classify(file)
             obj = labels[0].description
+            '''
+
+            results = predictor.classify_image(file)
+            obj = results.predictions[0].tag_name
 
             return render(request, 'result.html', {'result' : obj})
     else:
